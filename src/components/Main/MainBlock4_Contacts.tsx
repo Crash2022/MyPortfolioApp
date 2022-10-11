542fa835ea99897918285a83016a4b5d1e5c934e
Crash2022 <crash555@mail.ru>
2022-10-11 14:20:52 +0500

    block 4 fixed

--- src/components/Main/MainBlock4_Contacts.tsx
+++ src/components/Main/MainBlock4_Contacts.tsx
@@ -4,11 +4,12 @@ import style from '../Main/MainBlock4.module.css'
 export const MainBlock4_Contacts = () => {
 
     return (
-        <div className={style.mainBlock4}>
-            <h2>Контакты</h2>
-            <input type="text"/>
-            <input type="text"/>
-            <textarea name="contacts"></textarea>
-        </div>
+            <form className={style.mainBlock4}>
+                <h2>Контакты</h2>
+                <input type="text"/>
+                <input type="text"/>
+                <textarea name="contacts"></textarea>
+                <button>Отправить</button>
+            </form>
     );
 }
