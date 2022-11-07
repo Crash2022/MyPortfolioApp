0ac769ddbdbabb0083230e04fcb54e9f8aa8969e
Crash2022 <crash555@mail.ru>
2022-11-07 13:47:49 +0500

    config media

--- src/components/Main/03_Projects/MainBlock3_Projects.tsx
+++ src/components/Main/03_Projects/MainBlock3_Projects.tsx
@@ -3,13 +3,17 @@ import style from './MainBlock3.module.css'
 import {v1} from 'uuid'
 import {ProjectsList} from './ProjectsList'
 import {Title} from '../../../common/components/Title'
-import projectSocial from '../../../assets/images/project_social.jpg'
+import projectSocial from '../../../assets/images/project_social2.jpg'
+import projectFriday from '../../../assets/images/project_friday1.jpg'
+import projectPortfolio from '../../../assets/images/project_portfolio.jpg'
+import projectTodolist from '../../../assets/images/project_todolist1.jpg'
 
 export type ProjectsArrayPropsType = {
     id: string
     style: StyleType
     title: string
     text: string
+    link: string
 }
 
 export type StyleType = {
@@ -21,21 +25,24 @@ export const MainBlock3_Projects = () => {
     const [projects, setProjects] = useState<Array<ProjectsArrayPropsType>>([
         {
             id: v1(), style: {backgroundImage: `url(${projectSocial})`}, title: 'Social Network',
-            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            link: 'https://github.com/Crash2022/IT-Samurai-Social'
         },
         {
-            id: v1(), style: {backgroundImage: `url(${projectSocial})`}, title: 'Friday Project',
-            text: 'Can make projects on Angular'
+            id: v1(), style: {backgroundImage: `url(${projectFriday})`}, title: 'Friday Project',
+            text: 'Can make projects on Angular', link: 'https://github.com/Crash2022/Samurai-Counter'
         },
         {
-            id: v1(), style: {backgroundImage: `url(${projectSocial})`}, title: 'Portfolio App',
+            id: v1(), style: {backgroundImage: `url(${projectPortfolio})`}, title: 'Portfolio App',
             text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, ' +
-                'culpa deserunt dolorem doloremque dolores ducimus eaque eos ex harum.'
+                'culpa deserunt dolorem doloremque dolores ducimus eaque eos ex harum.',
+            link: 'https://github.com/Crash2022/MyPortfolioApp'
         },
-        {id: v1(), style: {backgroundImage: `url(${projectSocial})`}, title: 'Todolist',
+        {id: v1(), style: {backgroundImage: `url(${projectTodolist})`}, title: 'Todolist',
             text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto, culpa deserunt ' +
                 'dolorem doloremque dolores ducimus eaque eos ex harum minus neque non optio porro possimus ' +
-                'qui recusandae repellat voluptate voluptatem?'
+                'qui recusandae repellat voluptate voluptatem?',
+            link: 'https://github.com/Crash2022/Samurai-TodoList'
         }
     ]);
 
--- src/components/Main/03_Projects/Project_Item.tsx
+++ src/components/Main/03_Projects/Project_Item.tsx
@@ -6,14 +6,15 @@ type ProjectItemPropsType = {
     title: string
     text: string
     style: StyleType
+    link: string
 }
 
-export const Project_Item: React.FC<ProjectItemPropsType> = ({title, text, style}) => {
+export const Project_Item: React.FC<ProjectItemPropsType> = ({title, text, style, link}) => {
 
     return (
         <>
             <div className={styles.item_image} style={style}>
-                <a href="#">Посмотреть</a>
+                <a className={styles.item_button} href={link}>Посмотреть</a>
             </div>
             <div className={styles.item_title}><h3>{title}</h3></div>
             <div className={styles.item_description}>{text}</div>
