62f8b867e3a67a48c0f33bf0b5c013f3647b4b4f
Crash2022 <crash555@mail.ru>
2022-11-21 22:00:04 +0500

    change block1 styles

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import './App.css'
 import {Header} from "./components/Header/Header"
 import {Main} from "./components/Main/00_Main/Main"
 import {Footer} from "./components/Footer/Footer";
-import {TopButton} from "./common/components/TopButton";
 
 export const App = () => {
     return (
@@ -11,7 +10,6 @@ export const App = () => {
             <Header/>
             <Main/>
             <Footer/>
-            {/*<TopButton/>*/}
         </div>
     );
 }
