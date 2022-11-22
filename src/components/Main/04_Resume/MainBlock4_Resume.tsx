8ae1980fd6c40c0af9a4941564340361fa3c2f8e
Crash2022 <crash555@mail.ru>
2022-11-22 14:11:59 +0500

    fix resume part

--- src/components/Main/04_Resume/MainBlock4_Resume.tsx
+++ src/components/Main/04_Resume/MainBlock4_Resume.tsx
@@ -4,34 +4,39 @@ import style from './MainBlock4.module.scss'
 import {Title} from '../../../common/components/Title'
 import working from '../../../assets/images/photos/photo_resume_03.jpg'
 import {Button} from "../../../common/components/Button";
+import s from "../00_Main/Main.module.css";
 
 export const MainBlock4_Resume = () => {
 
     return (
         <div className={style.mainBlock4} id='resume'>
-            <Title title={'Резюме'}/>
-            <div className={style.mainBlock4_resume}>
+            <div className={s.container}>
 
-                <div className={style.mainBlock4_photo}>
-                    <img src={working} alt="photo_resume"/>
-                </div>
+                <Title title={'Резюме'}/>
 
-                <div className={style.mainBlock4_text}>
-                    <div className={style.mainBlock4_text_item}>
-                        <div className={style.mainBlock4_text_title}>
-                            React-разработчик
-                        </div>
-                        <div className={style.mainBlock4_text_body}>
-                            Мой стэк знания технологий, а также многолетний опыт позволяют
-                            разрабатывать веб приложения различного уровня и потребностей
-                        </div>
-                        <div className={style.mainBlock4_text_file}>
-                            <Button title={'Скачать резюме'}/>
+                <div className={style.mainBlock4_resume}>
+                    <div className={style.mainBlock4_photo}>
+                        <img src={working} alt="photo_resume"/>
+                    </div>
+
+                    <div className={style.mainBlock4_text}>
+                        <div className={style.mainBlock4_text_item}>
+                            <div className={style.mainBlock4_text_title}>
+                                React-разработчик
+                            </div>
+                            <div className={style.mainBlock4_text_body}>
+                                Мой стэк знания технологий, а также многолетний опыт позволяют
+                                разрабатывать веб приложения различного уровня и потребностей
+                            </div>
+                            <div className={style.mainBlock4_text_file}>
+                                <Button title={'Скачать резюме'}/>
+                            </div>
                         </div>
                     </div>
                 </div>
 
             </div>
+
         </div>
     );
 }
