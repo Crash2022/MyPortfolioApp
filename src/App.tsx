b42e02d5ce10cca28bef8e69c60ecee81dad00a4
Crash2022 <crash555@mail.ru>
2023-02-07 18:29:47 +0500

    add: vercel link

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import {Header} from "./features/Header/Header"
 import {Main} from "./features/Main/00_Main/Main"
 import {Footer} from "./features/Footer/Footer";
 
+// https://my-portfolio-app-beryl.vercel.app/
+
 export const App = () => {
     return (
         <div className="App">
