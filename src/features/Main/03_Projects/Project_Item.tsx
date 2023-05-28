7554031a4fbb85bdbba4e2adb08cb71d1cdd70ca
Crash2022 <crash555@mail.ru>
2023-05-28 23:02:35 +0500

    refactor: top button

--- src/features/Main/03_Projects/Project_Item.tsx
+++ src/features/Main/03_Projects/Project_Item.tsx
@@ -17,7 +17,7 @@ export const Project_Item: React.FC<ProjectItemPropsType> = ({title, text, style
     return (
         <>
             <div className={s.item_image} style={style}>
-                <a className={s.item_button} href={link}>{t('Go')}</a>
+                <a className={s.item_button} href={link} target={'_blank'}>{t('Go')}</a>
             </div>
             <div className={s.item_title}><h3>{title}</h3></div>
             <div className={s.item_description}>{text}</div>
--- src/shared/ui/TopButton/TopButton.tsx
+++ src/shared/ui/TopButton/TopButton.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import styles from "./TopButton.module.scss";
+import styles from "./TopButton.module.scss"
+import ArrowUp from '../../assets/icons/decor/arrow-up.svg'
 
 export const TopButton = () => {
 
     return (
             <a href="src/shared/ui/TopButton/TopButton#main" className={styles.topBtn}>
-                top
+                {/*top*/}
+                <img src={ArrowUp} alt='arrow-up' width={20} height={20}/>
             </a>
-    );
+    )
 }
