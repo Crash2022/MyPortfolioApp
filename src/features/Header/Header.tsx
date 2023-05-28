8bd6e52b84e23a678f360c70f04eb17725826b6f
Crash2022 <crash555@mail.ru>
2023-05-28 23:19:40 +0500

    refactor: header lang

--- src/features/Header/Header.tsx
+++ src/features/Header/Header.tsx
@@ -1,20 +1,14 @@
 import React from 'react'
 import style from './Header.module.scss'
 import {LangSwitcher} from '../../shared/ui/LangSwitcher/LangSwitcher'
-import {useTranslation} from 'react-i18next'
+import {NavLinks} from './NavLinks'
 
 export const Header = () => {
-
-    const {t} = useTranslation('nav-menu')
 
     return (
         <div className={style.header} id='main'>
             <nav className={style.navMenu}>
-                <a href="#main">{t('Home')}</a>
-                <a href="#skills">{t('Skills')}</a>
-                <a href="#projects">{t('Projects')}</a>
-                <a href="#resume">{t('CV')}</a>
-                <a href="#contacts">{t('Contacts')}</a>
+                <NavLinks/>
                 <LangSwitcher/>
             </nav>
         </div>
---
+++ src/features/Header/NavLinks.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import s from './NavLinks.module.scss'
+import {useTranslation} from 'react-i18next'
+
+export const NavLinks = () => {
+
+    const {t} = useTranslation('nav-menu')
+
+    return (
+        <>
+            <a href="#main">{t('Home')}</a>
+            <a href="#skills">{t('Skills')}</a>
+            <a href="#projects">{t('Projects')}</a>
+            <a href="#resume">{t('CV')}</a>
+            <a href="#contacts">{t('Contacts')}</a>
+        </>
+    )
+}
