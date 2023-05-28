07531bfc1b3a9da15099f4c46937163218da9cc2
Crash2022 <crash555@mail.ru>
2023-05-28 23:05:36 +0500

    refactor: top button v2

--- src/shared/ui/TopButton/TopButton.tsx
+++ src/shared/ui/TopButton/TopButton.tsx
@@ -5,7 +5,7 @@ import ArrowUp from '../../assets/icons/decor/arrow-up.svg'
 export const TopButton = () => {
 
     return (
-            <a href="src/shared/ui/TopButton/TopButton#main" className={styles.topBtn}>
+            <a href="#main" className={styles.topBtn}>
                 {/*top*/}
                 <img src={ArrowUp} alt='arrow-up' width={20} height={20}/>
             </a>
