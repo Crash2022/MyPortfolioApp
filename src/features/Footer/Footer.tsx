d59871b5571b66819f08a14467452f3a34df03e1
Crash2023 <crash55@mail.ru>
2025-01-25 11:21:54 +0500

    Refactor skills content

--- src/features/Footer/Footer.tsx
+++ src/features/Footer/Footer.tsx
@@ -4,11 +4,11 @@ import s from '../Main/00_Main/Main.module.css'
 import {TopButton} from "../../shared/ui/TopButton/TopButton";
 import ReactLogo from '../../shared/assets/icons/skills/react_02.svg'
 import ReactRedux from '../../shared/assets/icons/skills/redux-logo_01.svg'
-import Angular from '../../shared/assets/icons/skills/angular.svg'
+import Antd from '../../shared/assets/icons/skills/antd.svg'
 import Typescript from '../../shared/assets/icons/skills/typescript_02.svg'
 import Jest from '../../shared/assets/icons/skills/jest.svg'
 import NextJS from '../../shared/assets/icons/skills/next-js_01.svg'
-import Gatsby from '../../shared/assets/icons/skills/gatsby_02.svg'
+import Material from '../../shared/assets/icons/skills/material-ui.svg'
 import {useTranslation} from 'react-i18next'
 
 export const Footer = () => {
@@ -29,13 +29,13 @@ export const Footer = () => {
                         <img src={Typescript} alt="ts"/>
                     </div>
                     <div className={style.footer_item}>
-                        <img src={Angular} alt="angular"/>
+                        <img src={Antd} alt="antd"/>
+                    </div>
+                    <div className={style.footer_item}>
+                        <img src={Material} alt="material"/>
                     </div>
                     <div className={style.footer_item}>
                         <img src={NextJS} alt="next-js"/>
-                    </div>
-                    <div className={style.footer_item}>
-                        <img src={Gatsby} alt="gatsby"/>
                     </div>
                     <div className={style.footer_item}>
                         <img src={Jest} alt="jest"/>
